606847266c9a85d7893d31b55c0b4eb3572a1f41
Gustavo Adolfo Jaramillo Zambrano <59814986+gustavojz78@users.noreply.github.com>
2021-10-09 18:56:57 -0300

    Add files via upload
    Probando

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -12,15 +12,21 @@
 # Ingrese primero su nombre y luego su apellido
 # Capture ambos datos e imprima su nombre completo
 print('Ingrese por consola su nombre/s:')
-nombre = str(input())
+nombre = str(input("por favor ingrese su nombre : "))
 
 print('Ingrese por consola su apellido/s:')
-apellido = str(input())
+apellido = str(input("ahora su apellido : "))
 
 # Imprima su nombre completo
-
+print("su nombre es : ", nombre, apellido)
 # Almacenar su nombre completo en una variable
 # nombre_completo = .....
-
+nombre_completo= nombre +apellido
 # Imprimir la cantidad de letras que posee su nombre completo
 # cantidad_letras = len(....)
+cantidad_letrasNA= len(nombre+apellido)
+cantidad_letrasN = len(nombre)
+cantidad_letrasA = len(apellido)
+print("su nombre completo tiene " , cantidad_letrasNA, "letras")
+print("su nombre tiene " , cantidad_letrasN , "letras")
+print("su apellido tiene " , cantidad_letrasA , "letras")
