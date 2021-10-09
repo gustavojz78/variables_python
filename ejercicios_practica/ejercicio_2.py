048d2b62d7d38fb82f728b08eb70617e7900052a
Gustavo Adolfo Jaramillo Zambrano <59814986+gustavojz78@users.noreply.github.com>
2021-10-09 18:45:14 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,17 +19,21 @@ numero_2 = int(input())
 
 # Alumno: Imprima en pantalla los dos números enteros solicitados
 # print(....)
-
+print("los numeros ingresados son el ", numero_1 , "y el ", numero_2 )
 # Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
 # numero_1, numero_2
 # Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
 # El resultado de sumar 4 y 2 es 6
 # NOTA: No coloque usted los nùmeros y resultados, use las variables
-
 # Suma
-
+suma = numero_1 + numero_2
+print("La suma de los numeros es :", suma)
 # Resta
-
+resta = numero_1 - numero_2
+print("La resta de los numeros es :", resta)
 # División
-
+division = numero_1/numero_2
+print("La division de los numeros es :", division)
 # Multiplicación
+multiplicacion = numero_1 * numero_2
+print("La multiplicacion de los numeros es :", multiplicacion)
